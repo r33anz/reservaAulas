d4faaf7c41cc5d1c81e51b6e69fd6ced96bee768
Jovetic0809 <josephcoria2002@gmail.com>
2024-03-25 14:28:55 -0400

    Periodo Controller

--- apiReserva/app/Models/Periodo.php
+++ apiReserva/app/Models/Periodo.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Periodo extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
         'horainicial',
         'horafinal',
--- apiReserva/routes/api.php
+++ apiReserva/routes/api.php
@@ -11,7 +11,6 @@ use App\Models\Bloque;
 use App\Models\Piso;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 
 // /Docente
 Route::get('/docentes/{id}', [DocenteController::class, 'getMaterias']);
@@ -42,30 +41,3 @@ Route::get('/ambientes', [AmbienteController::class, 'index']);
 Route::post('/inhabilitarAmbiente', [InhabilitadoController::class, 'inhabilitarAmbiente']);
 Route::delete('/habilitarAmbiente', [InhabilitadoController::class, 'habilitarAmbiente']);
 Route::get('/buscarInhabilitados', [InhabilitadoController::class, 'buscarPeriodos']);
-=======
-use App\Models\Bloque;
-use App\Http\Controllers\DocenteController;
-
-
-///Docente
-
-Route::get('/docentes/{id}', [DocenteController::class, 'getMaterias']);
-
-
-///Administrador
-
-Route::get('/pisos', function (Request $request) {
-    return new BloqueResource(Bloque::find(1)); 
-});
-Route::get('/ambientes/pisos', function (Request $request) {
-    return new PisoResource(Piso::find(1)); 
-});
-
-Route::get('/bloques', [BloqueController::class, 'index']);
-Route::get('/bloques/{id}', [BloqueController::class, 'show']);
-
-Route::get('/ambientes', [AmbienteController::class, 'index']);
-Route::get('/ambientes/{id}', [AmbienteController::class, 'show']);
-///Registro de ambiente
-Route::post('/registroAmbiente',[AmbienteController::class,'store']);
->>>>>>> 0cc8690 ([ADD] ReservaAmbiente Controller)
