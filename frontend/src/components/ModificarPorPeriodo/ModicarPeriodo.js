24ca6eb90385ea07ab4bf06381fa62117aff7af9
drakomu <marco.an.me.fu@gmail.com>
2024-04-02 11:32:19 -0400

    modificar un ambiente por periodo

--- frontend/src/components/ModificarPorPeriodo/ModicarPeriodo.js
+++ frontend/src/components/ModificarPorPeriodo/ModicarPeriodo.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Button } from 'react-bootstrap';
-import { buscarAmbientePorNombre, modificarPerio } from '../../services/ModificarPeriodo.service';
+import { buscarAmbientePorNombre, modificarPerio ,estadoinhabilitado,habilita} from '../../services/ModificarPeriodo.service';
 import './stily.css';
 
 const Modificarperdiodo = () => {
@@ -14,6 +14,7 @@ const Modificarperdiodo = () => {
     const [idAmbiente, setId] = useState(''); // Estado para almacenar el ID del ambiente
     const [nombre, setNombre] = useState(''); // Estado para almacenar el nombre del ambiente
     const [periodosSeleccionados, setPeriodosSeleccionados] = useState([]); // Estado para almacenar los periodos seleccionados
+    const [periodosModificados, setPeriodosModificados] = useState([]);
 
     // Función para buscar ambientes por nombre
     const buscarAmbiente = (nombre) => {
@@ -33,12 +34,13 @@ const Modificarperdiodo = () => {
     };
 
     // Función para modificar periodo
-    const modifica = () => {
-        modificarPerio()
+    const modifica = (idAmbiente,fecha) => {
+        modificarPerio(idAmbiente,fecha)
             .then(data => {
                 // Obtener solo la hora inicial de cada periodo y actualizar las opciones de periodo
-                setPeriodosOptions(data);
-                console.log(periodosOptions);
+                setPeriodosModificados(data.periodos);
+                cambiarColorLabels();
+                
             })
             .catch(error => {
                 console.log('Error al buscar los ambientes:', error);
@@ -60,20 +62,70 @@ const Modificarperdiodo = () => {
         setNombreAmbiente(selectedNombre);
         setAmbienteOptions([]); // Limpiar las opciones de ambiente después de seleccionar una
     };
-
     const handleConsultarClick = () => {
         // Verificar que se haya seleccionado un ambiente y que se haya ingresado una fecha
         if (idAmbiente && fecha) {
             // Aquí puedes usar el ID del ambiente (almacenado en el estado 'id') y la fecha (almacenada en el estado 'fecha')
             console.log("ID del ambiente:", idAmbiente);
             console.log("Fecha ingresada:", fecha);
-            
+            modifica(idAmbiente,fecha);
             // Aquí puedes realizar cualquier otra operación que necesites con el ID y la fecha, como enviarlos a un servicio para obtener resultados específicos, etc.
     
         } else {
             console.log("Debes seleccionar un ambiente e ingresar una fecha para realizar la consulta.");
         }
     };
+
+    const estado = () => {
+        const checkboxes = document.querySelectorAll('.periodos-container input[type="checkbox"]');
+        const seleccionados = [];
+        
+        checkboxes.forEach(checkbox => {
+            if (checkbox.checked) {
+                seleccionados.push({ id: checkbox.id });
+                console.log('periodosModificados:', periodosModificados);
+                console.log('checkbox.id:', checkbox.id);
+                console.log('checkbox.checked:', checkbox.checked);
+                
+                // Convertir checkbox.id a número
+                const checkboxIdAsNumber = parseInt(checkbox.id, 10);
+                // Verifica si el ID del checkbox está en periodosModificados
+                if (periodosModificados.includes(checkboxIdAsNumber)) {
+                    console.log("El ID del checkbox está en periodosModificados");
+                    otraHabilitar(checkbox.id);
+                } else {
+                    console.log("El ID del checkbox no está en periodosModificados");
+                    otraFuncion(checkbox.id);
+                }
+                
+                // Llama a tu función y pasa el id como argumento
+                // otraFuncion(checkbox.id);
+            }
+        });
+        modifica(idAmbiente,fecha);
+        setPeriodosSeleccionados(seleccionados);
+    };
+    
+const otraHabilitar = (id) => {
+        const ids = [id];
+        habilita(idAmbiente,ids,fecha)
+        
+        // Llama a otras funciones aquí si es necesario
+    };
+    const otraFuncion = (id) => {
+        const ids = [id];
+        estadoinhabilitado(idAmbiente,ids,fecha)
+        // Llama a otras funciones aquí si es necesario
+    };
+
+    const cambiarColorLabels = useCallback((periodo) => {
+        // Verificar si el periodo está modificado
+        const periodoModificado = periodosModificados.includes(periodo);
+        // Devolver una clase CSS dependiendo del estado del periodo
+        return periodoModificado ? 'periodos-inhabilitados' : 'periodos-habilitados';
+    }, [periodosModificados]);
+    
+    
 
     return (
         <div className="buscar-container">
@@ -109,102 +161,103 @@ const Modificarperdiodo = () => {
                     value={fecha}
                     onChange={(e) => setFecha(e.target.value)}
                 />
-            </div >
+            </div>
             <Button className="consultar-button" onClick={handleConsultarClick}>Consultar</Button>
             <div className="periodos-container">
             <h1>Periodos:</h1>
                 <div>
                     <input
                         type="checkbox"
-                        id="periodo1"
+                        id="1"
                         name="periodo1"
                         value="periodo1"
                     />
-                    <label htmlFor="periodo1">6:45-8:15</label>
+                    <label htmlFor="periodo1" className={cambiarColorLabels(1)}>6:45-8:15</label>
                 </div>
                 <div>
                     <input
                         type="checkbox"
-                        id="periodo2"
+                        id="2"
                         name="periodo2"
                         value="periodo2"
                     />
-                    <label htmlFor="periodo2">8:15-9:45</label>
+                    <label htmlFor="periodo2" className={cambiarColorLabels(2)} >8:15-9:45</label>
                 </div>
                 <div>
                     <input
                         type="checkbox"
-                        id="periodo3"
+                        id="3"
                         name="periodo3"
                         value="periodo3"
                     />
-                    <label htmlFor="periodo3">9:45-11:15</label>
+                    <label htmlFor="periodo3" className={cambiarColorLabels(3)} >9:45-11:15</label>
                 </div>
                 <div>
                     <input
                         type="checkbox"
-                        id="periodo4"
+                        id="4"
                         name="periodo4"
                         value="periodo4"
                     />
-                    <label htmlFor="periodo4">11:15-12:45</label>
+                    <label htmlFor="periodo4" className={cambiarColorLabels(4)}>11:15-12:45</label>
                 </div>
                 <div>
                     <input
                         type="checkbox"
-                        id="periodo5"
+                        id="5"
                         name="periodo5"
                         value="periodo5"
                     />
-                    <label htmlFor="periodo5">12:45-14:15</label>
+                    <label  htmlFor="periodo5" className={cambiarColorLabels(5)}>12:45-14:15</label>
                 </div>
                 <div>
                     <input
                         type="checkbox"
-                        id="periodo6"
+                        id="6"
                         name="periodo6"
                         value="periodo6"
                     />
-                    <label htmlFor="periodo6">14:15-15:45</label>
+                    <label htmlFor="periodo6" className={cambiarColorLabels(6)}>14:15-15:45</label>
                 </div>
                 <div>
                     <input
                         type="checkbox"
-                        id="periodo7"
+                        id="7"
                         name="periodo7"
                         value="periodo7"
                     />
-                    <label htmlFor="periodo7">15:45-17:15</label>
+                    <label htmlFor="periodo7" className={cambiarColorLabels(7)}>15:45-17:15</label>
                 </div>
                 <div>
                     <input
                         type="checkbox"
-                        id="periodo8"
+                        id="8"
                         name="periodo8"
                         value="periodo8"
                     />
-                    <label htmlFor="periodo8">17:15-16:45</label>
+                    <label htmlFor="periodo8" className={cambiarColorLabels(8)}>17:15-16:45</label>
                 </div>
                 <div>
                     <input
                         type="checkbox"
-                        id="periodo9"
+                        id="9"
                         name="periodo9"
                         value="periodo9"
                     />
-                    <label htmlFor="periodo9">16:45-20:15</label>
+                    <label htmlFor="periodo9" className={cambiarColorLabels(9)}>16:45-20:15</label>
                 </div>
                 <div>
                     <input
                         type="checkbox"
-                        id="periodo10"
+                        id="10"
                         name="periodo10"
                         value="periodo10"
                     />
-                    <label htmlFor="periodo10">20:15-21:45</label>
+                    <label htmlFor="periodo10" className={cambiarColorLabels(10)}>20:15-21:45</label>
                 </div>
                 {/* Agrega más periodos aquí si es necesario */}
             </div>
+            <Button className="modi" onClick={estado}>Modificar</Button>
         </div>
     );
     
--- frontend/src/services/ModificarPeriodo.service.js
+++ frontend/src/services/ModificarPeriodo.service.js
@@ -14,11 +14,48 @@ export const buscarAmbientePorNombre = (nombre) => {
         });
 }
 
-export const modificarPerio = () => {
-    return axios.get(`${apiUrl}/periodos`)
+export const modificarPerio = (idAmbiente,fecha) => {
+    return axios.post(`${apiUrl}/buscarInhabilitados`,{idAmbiente:idAmbiente, fecha:fecha })
         .then(function (response) {
-            
+            console.log(response.data);
             return response.data; // Devuelve los datos del ambiente encontrado
+        })
+        .catch(function (error) {
+            console.log(error);
+            return null; // Manejo del error: retorna null si hay un error en la búsqueda
+        });
+}
+
+export const estadoinhabilitado = (idAmbiente, idPeriodos, fecha) => {
+    console.log(idAmbiente);
+    console.log(idPeriodos);
+    console.log(fecha);
+    return axios.post(`${apiUrl}/inhabilitarAmbiente`, {
+            idAmbiente: idAmbiente,
+            idPeriodos: idPeriodos,
+            fecha: fecha
+        })
+        .then(function (response) {
+            return response; // Devuelve los datos del ambiente encontrado
+        })
+        .catch(function (error) {
+            console.log(error);
+            return null; // Manejo del error: retorna null si hay un error en la búsqueda
+        });
+}
+
+export const habilita = (idAmbiente, idPeriodos, fecha) => {
+    
+    console.log(idAmbiente);
+    console.log(idPeriodos.map((item)=>parseInt(item)));
+    console.log(fecha);
+    return axios.delete(`${apiUrl}/habilitarAmbiente`, {data:{
+            idAmbiente: idAmbiente,
+            idPeriodos: idPeriodos.map((item)=>parseInt(item)),
+            fecha: fecha
+ } })
+        .then(function (response) {
+            return response; // Devuelve los datos del ambiente encontrado
         })
         .catch(function (error) {
             console.log(error);
