ada2f61ad77f9d16a1c40b64dde254e31a18c904
WebArtisan <henriquealvarezcorralesii@hotmail.com>
2024-04-03 02:10:17 -0400

    bug importacion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import Home from './pages/Home';
 import { AlertsProvider } from './components/Alert/AlertsContext';
 import Buscar from './components/Busquedanombre/Buscar';
 import ModificarPeriodo from './components/ModificarPorPeriodo/ModicarPeriodo';
-import RegistrarAmbiente from './components/RegistrarAmbiente';
 
 function App() {
 
