6bf5f6d9adaad2a954ca34c80288d5f874100fc7
WebArtisan <henriquealvarezcorralesii@hotmail.com>
2024-05-09 21:28:36 -0400

    Conectando historia con la base de Datos

---
+++ frontend/src/services/Fechas.service.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const apiUrl = process.env.REACT_APP_URL;
+
+export const recuperarFechasSolicitud = () => {
+    return axios.get(`${apiUrl}/fechasSolicitud`)
+        .then(function (response) {
+            console.log(response)
+            return response.data;
+        });
+}
+
+export const recuperarInformacionSolicitud = (identificador) => {
+    return axios.post(`${apiUrl}/informacionSolicitud`, { id: identificador })
+      .then(function (response) {
+        return response.data; // Devuelve los datos de la solicitud
+      })
+      .catch(function (error) {
+        console.log(error);
+        return null; // Manejo del error: retorna null si hay un error en la búsqueda
+      });
+  };
