a92f914a2599805a8ecafc70570dd50635974aa6
Cristal Flores <cristalflores.bo@gmail.com>
2024-05-15 09:49:10 -0400

    se agrego fecha atendida a rechazo

--- frontend/src/services/AtenderSolicitud.service.js
+++ frontend/src/services/AtenderSolicitud.service.js
@@ -42,7 +42,12 @@ export const aceptarSolicitud = (id) => {
 }
 
 export const rechazarSolicitud = (id, razonRechazo) => {
-    return axios.put(`${apiUrl}/rechazarSolicitud`, { id, razonRechazo })
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}`;
+    return axios.put(`${apiUrl}/rechazarSolicitud`, { id, razonRechazo, fechaAtendida: formattedDate })
         .then(function (response) {
             return response.data;
         })
