f88e0bd16cfb5c4bab9cebeeeac35c7bda99323c
Cristal Flores <cristalflores.bo@gmail.com>
2024-05-14 22:06:34 -0400

    [HU] Se actualizo la lista con el nuevo API de obtener solicitudes

--- frontend/src/services/Reserva.service.js
+++ frontend/src/services/Reserva.service.js
@@ -1,36 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
 const apiUrl = process.env.REACT_APP_URL;
 
-export const recuperarSolicitudesDeReserva = (id) => {
-    return axios.get(`${apiUrl}/solicitudesPorLlegada`)
-        .then(function (response) {
-            return response.data;
-        });
-}
+export const recuperarSolicitudesDeReserva = (pagina, estado) => {
+  return axios
+    .post(`${apiUrl}/verListas`, { pagina, estado })
+    .then(function (response) {
+      return response.data;
+    });
+};
 export const recuperarSolicitudesDeReservaAceptadas = (id) => {
-    return axios.get(`${apiUrl}/solicitudesAceptadas`)
-        .then(function (response) {
-            return response.data;
-        });
-}
+  return axios.get(`${apiUrl}/solicitudesAceptadas`).then(function (response) {
+    return response.data;
+  });
+};
 
 export const recuperarReservas = (id) => {
-    return axios.get(`${apiUrl}/docentes/${id}/reservas`)
-        .then(function (response) {
-            return response.data;
-        });
-}
+  return axios
+    .get(`${apiUrl}/docentes/${id}/reservas`)
+    .then(function (response) {
+      return response.data;
+    });
+};
 export const cancelarReserva = (id) => {
-    return axios.put(`${apiUrl}/reservas/${id}`)
-        .then(function (response) {
-            return response.data;
-        })
-        .catch(function (error) {
-            // Manejar cualquier error que ocurra durante la solicitud
-            console.error('Error al cancelar la reserva:', error);
-            throw error; // Lanzar el error para que pueda ser manejado en otro lugar si es necesario
-        });
-}
-
-
+  return axios
+    .put(`${apiUrl}/reservas/${id}`)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      // Manejar cualquier error que ocurra durante la solicitud
+      console.error("Error al cancelar la reserva:", error);
+      throw error; // Lanzar el error para que pueda ser manejado en otro lugar si es necesario
+    });
+};
