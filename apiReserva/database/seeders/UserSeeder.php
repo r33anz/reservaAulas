f2551740e6cc441844e856196feff0a7fdcec5c3
r33anz <anzaldoalvaradorodrigo@gmail.com>
2024-06-16 21:15:10 -0400

    [ADD] Login

--- apiReserva/database/seeders/UserSeeder.php
+++ apiReserva/database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 class UserSeeder extends Seeder
 {
     /**
@@ -17,37 +18,37 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Leticia Blanco',
             'email' => 'rodrigo33newton@gmail.com',
-            'password' =>'123'
+            'password' =>Hash::make('123')
         ]);
 
         User::create([
             'name' => 'Tatiana Aparicio',
             'email' => '202000717@est.umss.edu',
-            'password' => '123'
+            'password' => Hash::make('123')
         ]);
 
         User::create([
             'name' => 'Boris Calancha',
             'email' => '202000671@est.umss.edu',
-            'password' =>'123'
+            'password' =>Hash::make('123')
         ]);
 
         User::create([
             'name' => 'Indira Camacho',
             'email' => '201400072@est.umss.edu',
-            'password' => '123'
+            'password' => Hash::make('123')
         ]);
 
         User::create([
             'name' => 'Marcelo Flores',
             'email' => '202000321@est.umss.edu',
-            'password' =>'123'
+            'password' =>Hash::make('123')
         ]);
 
         User::create([
             'name' => 'Administrador 1',
             'email' => 'gestoradeambientesumss1@gmail.com',
-            'password' =>'123'
+            'password' =>Hash::make('123')
         ]);
 
     }
--- apiReserva/routes/api.php
+++ apiReserva/routes/api.php
@@ -16,7 +16,10 @@ use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\SolicitudController;
 use App\Http\Controllers\ValidadorController;
 use App\Http\Controllers\ReporteController;
+use App\Http\Controllers\AuthController;
 ///Docente
+
+
 Route::get('/docentes/{id}', [DocenteController::class, 'getMaterias']);  //REDONE
 Route::get('/listaDocentes', [DocenteController::class, 'getAllDocenteNames']); //REDONE
 
@@ -73,3 +76,77 @@ Route::post('/notificacionBroadcast',[NotificationController::class,'broadcast']
 
 //reportes
 Route::get('/generarReporte', [ReporteController::class, 'generarReporte']);
+
+
+
+/*
+//autentificacion
+// Rutas públicas
+Route::post('/login', [AuthController::class, 'login']);
+
+// Rutas protegidas
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // Docentes
+    Route::get('/docentes/{id}', [DocenteController::class, 'getMaterias']);
+    Route::get('/listaDocentes', [DocenteController::class, 'getAllDocenteNames']);
+
+    // Bloques
+    Route::get('/bloques', [BloqueController::class, 'index']);
+    Route::get('/bloques/{id}', [BloqueController::class, 'show']);
+
+    // Periodos
+    Route::get('/periodos/{id}', [PeriodoController::class, 'show']);
+    Route::get('/periodos', [PeriodoController::class, 'getPeriodos']);
+
+    // Ambientes
+    Route::post('/busquedaAula', [AmbienteController::class, 'buscar']);
+    Route::post('/busquedaAulaNew', [AmbienteController::class, 'buscarV2']);
+    Route::get('/ambiente/{id}', [AmbienteController::class, 'show']);
+    Route::get('/ambientes', [AmbienteController::class, 'index']);
+    Route::post('/registroAmbiente', [AmbienteController::class, 'store']);
+    Route::get('/{id}/ambientesMismoPiso', [AmbienteController::class, 'ambientesMismoPiso']);
+    Route::get('/{id}/ambientesMismoBloque', [AmbienteController::class, 'ambientesMismoBloque']);
+    Route::post('/buscarPorCapacidad', [AmbienteController::class, 'buscarPorCapacidad']);
+
+    // Habilitado/DeshabilitadoAula
+    Route::post('/inhabilitarAmbiente', [InhabilitadoController::class, 'inhabilitarAmbiente']);
+    Route::delete('/habilitarAmbiente', [InhabilitadoController::class, 'habilitarAmbiente']);
+    Route::post('/buscarInhabilitados', [InhabilitadoController::class, 'buscarPeriodos']);
+
+    // Solicitud
+    Route::get('/fechasSolicitud', [SolicitudController::class, 'conseguirFechas']);
+    Route::post('/realizarSolicitud', [SolicitudController::class, 'registroSolicitud']);
+    Route::post('/realizarSolicitudP2', [SolicitudController::class, 'registroSolicitudP2']);
+    Route::post('/informacionSolicitud', [SolicitudController::class, 'informacionSolicitud']);
+    Route::get('/{idSolicitud}/recuperarInformacion', [SolicitudController::class, 'recuperarInformacion']);
+    Route::put('/aceptarSolicitud', [SolicitudController::class, 'aceptarSolicitud']);
+    Route::put('/rechazarSolicitud', [SolicitudController::class, 'rechazarSolicitud']);
+    Route::post('/verListas', [SolicitudController::class, 'verListas']);
+    Route::get('/periodosSolicitados/{fecha}/{idAmbiente}', [SolicitudController::class, 'periodosSolicitados']);
+
+    // Validador
+    Route::post('/consultarFechaPeriodo', [ValidadorController::class, 'consultaFechaPeriodo']);
+    Route::post('/consultarFechaPeriodAmbiente', [ValidadorController::class, 'consultarFechaPeriodoAmbiente']);
+    Route::get('/solicitudAtendida/{idSolicitud}', [ValidadorController::class, 'SolicitudAtendida']);
+
+    // Reservas
+    Route::post('/docentes/reservas', [ReservaController::class, 'reservasPorDocente']);
+    Route::put('/reservas/{id}', [ReservaController::class, 'cancelarReserva']);
+    Route::get('/periodosReservados/{fecha}/{idAmbiente}', [ReservaController::class, 'periodosReservados']);
+    Route::put('/inhabilitarReserva', [ReservaController::class, 'inhabilitarReserva']);
+
+    // Notificaciones
+    Route::post('/marcarNotificacionLeida', [NotificationController::class, 'marcarNotificacionLeida']);
+    Route::get('/notificaciones/{idUsuario}', [NotificationController::class, 'recuperarNotificaciones']);
+    Route::post('/notificarIndividualmente', [NotificationController::class, 'notificacionIndividual']);
+    Route::post('/notificacionBroadcast', [NotificationController::class, 'broadcast']);
+
+    // Reportes
+    Route::get('/generarReporte', [ReporteController::class, 'generarReporte']);
+});*/
